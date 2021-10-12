d3b32d595bcb521205cfbcd34b4c1f156d092097
Gonzalo Martinez <gonzalo.martinez.larumbe@hp.com>
2021-10-12 15:42:25 +0200

    Initial commit

---
+++ libs/larumbe-functions.el
@@ -0,0 +1,390 @@
+;;; larumbe-functions.el --- Larumbe's functions  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+;;;; Window resizing
+(defvar larumbe/shrink-window-horizontally-delta   15)
+(defvar larumbe/shrink-window-vertically-delta      5)
+
+
+;;;###autoload
+(defun larumbe/enlarge-window-horizontally ()
+  "Use `shrink-window' as a wrapper."
+  (interactive)
+  (shrink-window larumbe/shrink-window-horizontally-delta t))
+
+
+;;;###autoload
+(defun larumbe/shrink-window-horizontally ()
+  "Use `shrink-window' as a wrapper."
+  (interactive)
+  (shrink-window (- larumbe/shrink-window-horizontally-delta) t))
+
+
+;;;###autoload
+(defun larumbe/enlarge-window-vertically ()
+  "Use `shrink-window' as a wrapper."
+  (interactive)
+  (shrink-window larumbe/shrink-window-vertically-delta))
+
+
+;;;###autoload
+(defun larumbe/shrink-window-vertically ()
+  "Use `shrink-window' as a wrapper."
+  (interactive)
+  (shrink-window (- larumbe/shrink-window-vertically-delta)))
+
+
+
+;;;; Buffer management
+;;;###autoload
+(defun larumbe/kill-current-buffer ()
+  "Kill current buffer without confirmation."
+  (interactive)
+  (kill-buffer (buffer-name)))
+
+
+(defvar larumbe/revert-buffer-confirm-p t
+  "Ask for confirmation when reverting current buffer.")
+
+;;;###autoload
+(defun larumbe/revert-buffer-maybe-no-confirm (toggle)
+  "Revert current buffer without prompting for confirmation.
+If universal arg or TOGGLE are provided, toggle for confirmation."
+  (interactive "P")
+  (if toggle
+      (if larumbe/revert-buffer-confirm-p
+          (progn
+            (setq larumbe/revert-buffer-confirm-p nil)
+            (message "Revert confirmation: nil"))
+        (setq larumbe/revert-buffer-confirm-p t)
+        (message "Revert confirmation: t"))
+    ;; If not toggling, revert
+    (if larumbe/revert-buffer-confirm-p
+        (revert-buffer nil nil t)
+      (revert-buffer nil t nil)))) ; When skipping confirmation, reload major-mode
+
+
+;;;###autoload
+(defun larumbe/current-buffer-to-file (out-file)
+  "Export current buffer to OUT-FILE.
+Seems useful to export long compilation logs."
+  (interactive "FEnter output path: ")
+  (append-to-file (point-min) (point-max) out-file))
+
+
+;;;###autoload
+(defun larumbe/pwd-to-kill-ring (&optional no-line)
+  "Copy current file path to `kill-ring'.
+If optional NO-LINE is given, then do not copy line to `kill-ring'"
+  (interactive "P")
+  (let (file-name)
+    (if no-line
+        (setq file-name (buffer-file-name))
+      (setq file-name (concat (buffer-file-name) ":" (format "%s" (line-number-at-pos)))))
+    (kill-new file-name)
+    (message (buffer-file-name))))
+
+
+
+;;;; Navigation
+;;;###autoload
+(defun larumbe/find-file-at-point ()
+  "Wrapper for `ffap' without asking for the file."
+  (interactive)
+  (let ((file (thing-at-point 'filename)))
+    (if (file-exists-p file)
+        (progn
+          (xref-push-marker-stack)
+          (ffap file))
+      (message "File \"%s\" does not exist (check point or current path)" file))))
+
+
+;;;###autoload
+(defun larumbe/pop-to-previous-mark ()
+  "Pop to previous mark."
+  (interactive)
+  (set-mark-command 4))
+
+
+;;;###autoload
+(defun larumbe/xref-find-definitions-at-point-dwim ()
+  "Find definition of symbol at point.
+If pointing a file, visit that file instead.
+
+INFO: Will use global/ggtags as a backend if configured."
+  (interactive)
+  (if (file-exists-p (thing-at-point 'filename))
+      (larumbe/find-file-at-point)
+    (xref-find-definitions (thing-at-point 'symbol))))
+
+
+;;;###autoload
+(defun larumbe/xref-find-reference-at-point ()
+  "Find reference of symbol at point.
+
+INFO: Will use global/ggtags as a backend if configured."
+  (interactive)
+  (xref-find-references (thing-at-point 'symbol)))
+
+
+
+;;;; Editing
+;;;###autoload
+(defun larumbe/copy-region-or-symbol-at-point ()
+  "Copy symbol under cursor.  If region is active, copy it instead.
+If there is no symbol at point, just skip functionality."
+  (interactive)
+  (let ((symbol (thing-at-point 'symbol t)))
+    (if (use-region-p)
+        (progn
+          (call-interactively #'kill-ring-save)
+          (deactivate-mark))
+      ;; If there is no region case, must be a symbol in order to do something relevant
+      (when symbol
+        (kill-new symbol)
+        (message symbol)))))
+
+
+;;;###autoload
+(defun larumbe/kill-sexp-backwards ()
+  "Kill sexp backwards."
+  (interactive)
+  (kill-sexp -1))
+
+
+;;;###autoload
+(defun larumbe/insert-time-stamp (&optional regex)
+  "Insert time-stamp at header comments.
+Try to add it in line before matching REGEX.
+If REGEX is nil or not found, add it at the beginning."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (when regex
+      (re-search-forward regex nil t))
+    (beginning-of-line)
+    (open-line 1)
+    (insert ";;\n;; Time-stamp: <>\n;; ")))
+
+
+;;;###autoload
+(defun larumbe/comment-tag-line-or-region (text-begin text-end)
+  "Surround current line or region with commented TEXT-BEGIN and TEXT-END.
+Makes use of `comment-dwim' because the approach of using the variables
+`comment-start' and `comment-end' was more complicated than it seemed.
+For example, `verilog-mode' or `python-mode' have no `comment-end' defined.
+This approach works well for line-style as well as for block-style comments
+(e.g. block-style /* Comment */ in C-mode). However, line-style are preferred."
+  (interactive)
+  (let (beg end)
+    (if (use-region-p)
+        (progn
+          (setq beg (region-beginning))
+          (setq end (region-end))
+          (deactivate-mark)
+          (goto-char end)
+          (beginning-of-line)
+          (open-line 1)
+          (call-interactively #'comment-dwim)
+          (insert text-end)
+          (goto-char beg)
+          (beginning-of-line)
+          (open-line 1)
+          (call-interactively #'comment-dwim)
+          (insert text-begin)
+          (forward-line 1))
+      ;; If not using the region wrap around current line
+      (beginning-of-line)
+      (open-line 1)
+      (call-interactively #'comment-dwim)
+      (insert text-begin)
+      (beginning-of-line)
+      (forward-line 2)
+      (open-line 1)
+      (call-interactively #'comment-dwim)
+      (insert text-end)
+      (forward-line -1))))
+
+
+;;;; Lists/regexp/strings/files/directories
+;;;###autoload
+(defun larumbe/print-elements-of-list-of-strings (list-of-strings)
+  "Print each element of LIST-OF-STRINGS on a line of its own."
+  (let (return-string)
+    (while list-of-strings
+      (setq return-string (concat return-string (message "%s\n" (car list-of-strings))))
+      (setq list-of-strings (cdr list-of-strings)))
+    (message "%s" return-string)))
+
+
+;;;###autoload
+(defun larumbe/insert-elements-of-list-of-strings (list-of-strings)
+  "Insert each element of LIST-OF-STRINGS on a line of its own at current point."
+  (dolist (elm list-of-strings)
+    (insert (concat elm "\n"))))
+
+
+;;;###autoload
+(defun larumbe/replace-regexp (regexp to-string start end)
+  "Wrapper function for programatic use of `replace-regexp'.
+Replace REGEXP with TO-STRING from START to END."
+  (save-excursion
+    (goto-char start)
+    (while (re-search-forward regexp end t)
+      (replace-match to-string))))
+
+
+;;;###autoload
+(defun larumbe/replace-regexp-whole-buffer (regexp to-string)
+  "Replace REGEXP with TO-STRING on whole current-buffer."
+  (larumbe/replace-regexp regexp to-string (point-min) nil))
+
+
+;;;###autoload
+(defun larumbe/replace-string (string to-string start end &optional fixedcase)
+  "Wrapper function for programatic use of `replace-string'.
+Replace STRING with TO-STRING from START to END.
+
+If optional arg FIXEDCASE is non-nil, do not alter the case of
+the replacement text (see `replace-match' for more info)."
+  (save-excursion
+    (goto-char start)
+    (while (search-forward string end t)
+      (replace-match to-string fixedcase))))
+
+
+;;;###autoload
+(defun larumbe/replace-string-whole-buffer (string to-string &optional fixedcase)
+  "Replace STRING with TO-STRING on whole current-buffer.
+
+If optional arg FIXEDCASE is non-nil, do not alter the case of
+the replacement text (see `replace-match' for more info)."
+  (larumbe/replace-string string to-string (point-min) nil fixedcase))
+
+
+
+;;;###autoload
+(defun larumbe/path-join (arg1 arg2)
+  "Join path of ARG1 and ARG2.
+If more than 2 args are required, use `f-join'"
+  (if (and arg1 arg2)
+      (concat (file-name-as-directory arg1) arg2)
+    (message "larumbe/path-join: Cannot join path with nil arguments.")
+    nil))
+
+
+;;;; Misc
+;;;###autoload
+(defun larumbe/toggle-keyboard-layout ()
+  "Toggle keyboard language between US and ES."
+  (interactive)
+  (let (cur-layout)
+    (setq cur-layout (shell-command-to-string "setxkbmap -query | grep layout | awk '{print $2}'"))
+    (setq cur-layout (replace-regexp-in-string "\n$" "" cur-layout))
+    (if (string-equal cur-layout "us")
+        (progn
+          (shell-command "setxkbmap es")
+          (message "Switched to ES"))
+      (shell-command "setxkbmap us")
+      (message "Switched to US"))))
+
+
+;;;; More complex/less frequently used
+;;;###autoload
+(defun larumbe/buffer-expand-filenames (&optional absolute)
+  "Expands filenames paths present in `current-buffer' line by line.
+If ABSOLUTE is set to non-nil filenames will expand to their absolute paths."
+  (interactive)
+  (let (cur-line)
+    (save-excursion
+      (goto-char (point-min))
+      (while (< (point) (point-max))
+        (delete-horizontal-space)
+        (if absolute
+            (setq cur-line (expand-file-name (thing-at-point 'line) default-directory))
+          (setq cur-line (file-relative-name (thing-at-point 'line) default-directory)))
+        (kill-line 1)
+        (insert cur-line)))))
+
+
+;;;###autoload
+(defun larumbe/sort-regexp-at-the-beginning-of-file (regexp)
+  "Move lines containing REGEXP recursively at the beginning of the file.
+Done line by line, this might be useful when managing a list of files,
+one file at a line, and there is some need of sorting by regexp.
+For example, in SystemVerilog, packages might need to be included before other files."
+  (interactive)
+  (let ((sorted-files-p nil))
+    (goto-char (point-min))
+    (unless sorted-files-p
+      (save-excursion
+        (unless (search-forward-regexp regexp nil 1)
+          (setq sorted-files-p t))
+        (beginning-of-line)
+        (kill-line 1)) ; Kill trailing newline as well
+      (yank))))
+
+
+
+;;;###autoload
+(defun larumbe/directory-files-recursively-to-file (base-dir file re &optional append exclude-re abs-path)
+  "Retrieve all files matching regexp RE of a specified BASE-DIR to output FILE.
+If optional APPEND is set to non-nil, append result to existing FILE.
+Otherwise, overwrite old existing FILE with new results.
+If optional EXCLUDE-RE is set, delete paths with that regexp from generated file.
+If ABS-PATH is set to non-nil the directory files will be shown as relative paths."
+  (let (buf)
+    (save-window-excursion
+      (with-temp-buffer
+        (mapc
+         (lambda (dir) (insert (mapconcat #'identity (directory-files-recursively dir re) "\n")))
+         (list base-dir))
+        (if abs-path
+            (larumbe/buffer-expand-filenames t)
+          (larumbe/buffer-expand-filenames))
+        ;; Append to existing file
+        (when (and (file-exists-p (concat base-dir file))
+                   append)
+          (setq buf (current-buffer))
+          (find-file file)
+          (goto-char (point-max))
+          (newline)
+          (insert-buffer-substring buf))
+        ;; Filter according to optional parameter
+        (when exclude-re
+          (flush-lines exclude-re (point-min) (point-max)))
+        (write-file file)))))
+
+
+;; https://stackoverflow.com/questions/3775377/how-do-you-diff-a-directory-for-only-files-of-a-specific-type
+;;;###autoload
+(defun larumbe/directory-diff-recursive (dir1 dir2 out-file)
+  "Export diff between DIR1 and DIR2 to OUT-FILE.
+It uses an exclude schema that leaves out of the diff
+the files/expresions in exclude.list This is because there
+is no include option for `diff' utils."
+  (interactive "DSelect first directory: \nDSelect second directory: \nFSelect output file:")
+  (let ((exclude-file)
+        (exclude-patterns '("*.wdf"
+                            "*.xml"
+                            "*.bxml"
+                            "*.wpc"
+                            "*.target"
+                            "*.rdl.ast"
+                            "file_list.py"
+                            "source_list.tcl"
+                            "run_vivado.tcl")))
+    (setq exclude-file (concat (file-name-directory out-file) "exclude.pats"))
+    (f-write-text (larumbe/print-elements-of-list-of-strings exclude-patterns) 'utf-8 exclude-file)
+    ;; If return value is `1' is because differences were found
+    (start-process-shell-command
+     "*diff-dirs*" nil
+     (concat "diff -X " exclude-file " -r " dir1 " " dir2 " > " out-file))))
+
+
+(provide 'larumbe-functions)
+
+
+;;; larumbe-functions.el ends here
---
+++ site-lisp/others-functions.el
@@ -0,0 +1,223 @@
+a;;; others-functions.el --- Third party custom functions  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; This file hosts some general purpose functions that I found over the Internet
+;;
+;; These are meant to be used all along the configuration file.
+;;
+;;; Code:
+
+(require 'f)
+(require 'with-editor)
+(require 'xref)
+
+;;;; Restart code
+;; https://emacs.stackexchange.com/questions/5428/restart-emacs-from-within-emacs
+(defun launch-separate-emacs-in-terminal ()
+  (suspend-emacs "fg ; emacs -nw"))
+
+
+(defun launch-separate-emacs-under-x ()
+  (call-process "sh" nil nil nil "-c" "emacs &"))
+
+
+;;;###autoload
+(defun restart-emacs ()
+  (interactive)
+  ;; We need the new emacs to be spawned after all kill-emacs-hooks
+  ;; have been processed and there is nothing interesting left
+  (let ((kill-emacs-hook (append kill-emacs-hook (list (if (display-graphic-p)
+                                                           #'launch-separate-emacs-under-x
+                                                         #'launch-separate-emacs-in-terminal)))))
+    (save-buffers-kill-emacs)))
+
+
+;;;; Buffer management
+;;;###autoload
+(defun close-all-buffers ()
+  "Kill all buffers."
+  (interactive)
+  (mapc #'kill-buffer (buffer-list)))
+
+
+;;;###autoload
+(defun only-current-buffer ()
+  "Kill all buffers except active one."
+  (interactive)
+  (mapc #'kill-buffer (cdr (buffer-list (current-buffer)))))
+
+
+;;;###autoload
+(defun buffer-mode (&optional buffer)
+  "Return the major mode associated with BUFFER."
+  (let (buf)
+    (if buffer
+        (setq buf buffer)
+      (setq buf (current-buffer)))
+    (with-current-buffer buf
+      major-mode)))
+
+
+;;;###autoload
+(defun file-title ()
+  "Return file title; e.g. for '/opt/asdf.txt' eval 'asdf'."
+  (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))
+
+
+
+;;;; Editing
+;; http://tuxicity.se/emacs/elisp/2010/03/11/duplicate-current-line-or-region-in-emacs.html
+;;;###autoload
+(defun duplicate-current-line-or-region (arg)
+  "Duplicates the current line or region ARG times.
+
+If there's no region, the current line will be duplicated. However, if
+there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (let (beg end (origin (point)))
+    (if (and mark-active (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if mark-active
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (let ((region (buffer-substring-no-properties beg end)))
+      (dotimes (i arg)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point)))
+      (goto-char (+ origin (* (length region) arg) arg)))))
+
+
+;;;; Lists/regexp/strings/files/directories
+;; http://ergoemacs.org/emacs/elisp_read_file_content.html
+;;;###autoload
+(defun read-lines (filePath)
+  "Return a list of lines of a file at FILEPATH."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (split-string (buffer-string) "\n" t)))
+
+
+;; http://ergoemacs.org/emacs/elisp_read_file_content.html
+;;;###autoload
+(defun get-string-from-file (filePath)
+  "Return FILEPATH file content as a string."
+  (with-temp-buffer
+    (insert-file-contents filePath)
+    (buffer-string)))
+
+
+;; https://stackoverflow.com/questions/17325713/looking-for-a-replace-in-string-function-in-elisp
+;;;###autoload
+(defun replace-in-string (what with in)
+  "Replace WHAT to WITH in string IN."
+  (replace-regexp-in-string (regexp-quote what) with in nil 'literal))
+
+
+
+;;;; More complex/less frequently used
+;;;###autoload
+(defun forward-same-indent ()
+  "Move forward to next line with same indent (copied from `vhdl-mode').
+DANGER: Comment needs to be substituted from '--' to  mode-specific comment."
+  (interactive)
+  (let ((pos (point))
+        (indent (current-indentation)))
+    (beginning-of-line 2)
+    (while (and (not (eobp))
+                (or (looking-at "^\\s-*\\(--.*\\)?$")
+                    (> (current-indentation) indent)))
+      (beginning-of-line 2))
+    (if (= (current-indentation) indent)
+        (back-to-indentation)
+      (message "No following line with same indent found in this block")
+      (goto-char pos)
+      nil)))
+
+
+;;;###autoload
+(defun backward-same-indent ()
+  "Move backward to previous line with same indent (copied from `vhdl-mode').
+DANGER: Comment needs to be substituted from '--' to  mode-specific comment."
+  (interactive)
+  (let ((pos (point))
+        (indent (current-indentation)))
+    (beginning-of-line -0)
+    (while (and (not (bobp))
+                (or (looking-at "^\\s-*\\(--.*\\)?$")
+                    (> (current-indentation) indent)))
+      (beginning-of-line -0))
+    (if (= (current-indentation) indent)
+        (back-to-indentation)
+      (message "No preceding line with same indent found in this block")
+      (goto-char pos)
+      nil)))
+
+
+;; https://emacs.stackexchange.com/questions/5441/function-to-delete-all-comments-from-a-buffer-without-moving-them-to-kill-ring
+;;;###autoload
+(defun delete-comments-from-buffer ()
+  "Delete comments from buffer without moving them to the kill ring."
+  (interactive)
+  (goto-char (point-min))
+  (let (kill-ring)
+    (comment-kill (count-lines (point-min) (point-max)))))
+
+
+;; https://stackoverflow.com/questions/31767779/is-there-an-apply-command-to-each-line-in-region-in-emacs
+;; INFO: Do not use functions that alter the length of the buffer
+;; (e.g. #'kill-line) as the start/end parameters will change during execution.
+;;;###autoload
+(defun do-lines (fun &optional start end)
+  "Invoke function FUN on the text of each line from START to END."
+  (interactive
+   (let ((fn (intern (completing-read "Function: " obarray 'functionp t))))
+     (if (use-region-p)
+         (list fn (region-beginning) (region-end))
+       (list fn (point-min) (point-max)))))
+  (save-excursion
+    (goto-char start)
+    (while (< (point) end)
+      (funcall fun (buffer-substring (line-beginning-position) (line-end-position)))
+      (forward-line 1))))
+
+
+
+;; https://gist.github.com/ffevotte/9345586#file-gistfile1-el
+;;;###autoload
+(defun source (filename)
+  "Update environment variables from FILENAME source file."
+  (interactive "fSource file: ")
+  (message "Sourcing environment from `%s'..." filename)
+  (with-temp-buffer
+    (shell-command (format "diff -u <(true; export) <(source %s; export)" filename) '(4))
+    (let ((envvar-re "declare -x \\([^=]+\\)=\\(.*\\)$"))
+      ;; Remove environment variables
+      (while (search-forward-regexp (concat "^-" envvar-re) nil t)
+        (let ((var (match-string 1)))
+          (message "%s" (prin1-to-string `(setenv ,var nil)))
+          (setenv var nil)))
+      ;; Update environment variables
+      (goto-char (point-min))
+      (while (search-forward-regexp (concat "^+" envvar-re) nil t)
+        (let ((var (match-string 1))
+              (value (read (match-string 2))))
+          (message "%s" (prin1-to-string `(setenv ,var ,value)))
+          (setenv var value)))))
+  (message "Sourcing environment from `%s'... done." filename))
+
+
+;; https://emacs.stackexchange.com/questions/10077/how-to-edit-crontab-directly-within-emacs-when-i-already-have-emacs-open
+;;;###autoload
+(defun crontab-e ()
+  "Run `crontab -e' in an Emacs buffer."
+  (interactive)
+  (with-editor-async-shell-command "crontab -e"))
+
+
+
+(provide 'others-functions)
+
+;;; others-functions.el ends here
