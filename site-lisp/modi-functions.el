c097360497b2b28ae2876db034278adc605db9d8
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2022-06-16 14:17:17 +0200

    Add modi htmlize leuven css based function

--- site-lisp/modi-functions.el
+++ site-lisp/modi-functions.el
@@ -114,6 +114,75 @@ the cons elements are strings."
 
 
 
+;; https://emacs.stackexchange.com/questions/14403/how-can-i-copy-syntax-highlighted-code-as-rtf-or-html
+(defvar modi/htmlize-output-directory
+  (let ((dir (concat temporary-file-directory
+                     (getenv "USER") "/.htmlize/"))) ; must end with /
+    (make-directory dir :parents)
+    dir)
+  "Output directory for files exported by `modi/htmlize-region-to-file'.")
+
+(defvar modi/htmlize-css-file (concat straight-base-dir "straight/repos/my-elisp-packages/site-lisp/leuven_theme.css")
+  "CSS file to be embedded in the html file created using the `modi/htmlize-region-to-file' function.")
+
+;;;###autoload
+(defun modi/htmlize-region-to-file (option)
+  "Export the selected region to an html file. If a region is not
+selected, export the whole buffer.
+
+The output file is saved to `modi/htmlize-output-directory' and its fontification
+is done using `modi/htmlize-css-file'.
+
+If OPTION is non-nil (for example, using `\\[universal-argument]' prefix), copy
+the output file name to kill ring.
+If OPTION is \\='(16) (using `\\[universal-argument] \\[universal-argument]' prefix),
+do the above and also open the html file in the default browser."
+  (interactive "P")
+  (require 'ox-html) ; INFO: Added by Larumbe
+  (let ((org-html-htmlize-output-type 'css)
+        (org-html-htmlize-font-prefix "org-")
+        (fname (concat modi/htmlize-output-directory
+                       (if (buffer-file-name)
+                           (file-name-nondirectory (buffer-file-name))
+                         "temp")
+                       ".html"))
+        start end html-string)
+    (if (use-region-p)
+        (progn
+          (setq start (region-beginning))
+          (setq end (region-end)))
+      (progn
+        (setq start (point-min))
+        (setq end (point-max))))
+    (setq html-string (org-html-htmlize-region-for-paste start end))
+    (with-temp-buffer
+      ;; Insert the `modi/htmlize-css-file' contents in the temp buffer
+      (insert-file-contents modi/htmlize-css-file nil nil nil :replace)
+      ;; Go to the beginning of the buffer and insert comments and
+      ;; opening tags for `html', `head' and `style'. These are
+      ;; inserted *above* the earlier inserted css code.
+      (goto-char (point-min))
+      (insert (concat "<!-- This file is generated using the "
+                      "`modi/htmlize-region-to-file' function\n"
+                      "from https://github.com/kaushalmodi/.emacs.d/"
+                      "blob/master/setup-files/setup-org.el -->\n"))
+      (insert "<html>\n<head>\n<style media=\"screen\" type=\"text/css\">\n")
+      ;; Go to the end of the buffer (end of the css code) and
+      ;; insert the closing tags for `style' and `head' and opening
+      ;; tag for `body'.
+      (goto-char (point-max))
+      (insert "</style>\n</head>\n<body>\n")
+      ;; Insert the HTML for fontified text in `html-string'.
+      (insert html-string)
+      ;; Close the `body' and `html' tags.
+      (insert "</body>\n</html>\n")
+      (write-file fname)
+      (when option
+        (kill-new fname)
+        (when (= 16 (car option))
+          (browse-url-of-file fname))))))
+
+
 
 (provide 'modi-functions)
 
